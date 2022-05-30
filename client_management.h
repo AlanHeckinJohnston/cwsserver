dfa74eab1921196d55ff28afc58c6fe3265266e5
AlanHeckinJohnston <31631811+AlanHeckinJohnston@users.noreply.github.com>
2022-05-29 23:36:58 -0400

    build tools, refactoring, and handshake logic

--- client_management.h
+++ client_management.h
@@ -1,4 +1,7 @@
+#ifndef CLIENT_MANAGEMENT_H
+#define CLIENT_MANAGEMENT_H
 #include <winsock2.h>
+#define SOCKET_TIMEOUT 10
 
 struct SocketInfo {
     SOCKET socket;
@@ -14,3 +17,5 @@ int readFromClients(struct SocketInfo** sockets, fd_set* read, int readInfo, int
 int acceptClients(SOCKET* listenSocketPtr, struct SocketInfo** sockets, int* connectedClients);
 void closeInactive(struct SocketInfo** sockets, int* result);
 void printMessages(struct SocketInfo** sockets);
+
+#endif
---
+++ tests/headerprocessor/headerprocessor_test.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    FILE *file = fopen("header_test.txt", "r");
+
+    
+    char* s = malloc(sizeof(char)*5000);
+    s[0] = '\0';
+
+    int end = 0;
+    do
+    {
+        char line[500];
+        fgets(line, 500, file);
+
+        end = feof(file);
+
+        if (!end)
+            strcat(s,line);
+    } while (!end);
+
+
+    fclose(file);
+
+    char** headers = malloc(sizeof(char*)*45);
+
+    if (headers == NULL)
+    {
+        printf("Ran out of memory :(");
+        return 1;
+    }
+
+    int headercount = getHeaders(s,headers);
+    printf("%d headers loaded\n", headercount);
+    for (int i = 0; i < headercount; i++)
+    {
+        printf("%d - %s\n",i, headers[i]);
+    }
+
+
+    char* host = getHeaderValue(headers, headercount, "Host");
+
+    if (host != NULL)
+    {
+        printf("%s", host);
+    }
+}
